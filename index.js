41a5ab71fa5bb4f5e5c4759d896e0c3a77e0c6dc
LeMinhQuan25 <quanle2502@gmail.com>
2023-04-05 13:22:28 +0700

    Làm middewares cho Project

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
 import express from "express";
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const app = express();
 const port = 1812;
+
+const flimRouter = require("./routes/flimRoutes");
+const userRouter = require("./routes/userRoutes");
+
+// Set up session middleware
+const mongoStore = new MongoStore({
+  mongooseConnection: mongoose.connection,
+  collection: "sessions",
+});
+
+app.use(
+  session({
+    secret: "mySecret",
+    resave: false,
+    saveUninitialized: true,
+    store: mongoStore,
+  })
+);
+
+// Set up body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Set up static files middleware
+app.use(express.static(path.join(__dirname, "public")));
+
+// Set up routeser);
+app.use("/flim", flimRouter);
+app.use("/user", userRouter);
+
+//Bắt đầu một session
+app.use((req, res, next) => {
+  req.session = req.session || {};
+  next();
+});
+app.use(
+  session({
+    secret: "your_secret_key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // get "/" là lấy route mặc định trang chủ
 app.get("/", (req, res) => {
