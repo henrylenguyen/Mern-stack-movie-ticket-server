d8d8419ba405bcb7e156be8e566609221b62ca3a
Le Nguyen Phuong Thai <thailnp133@gmail.com>
2023-04-07 16:02:47 +0700

    fix routes

--- routes/banner.routes.js
+++ routes/banner.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { layDanhSachBanner } from "../controllers/banner.controller.js";
+import { kiemTra } from './../utils/config';
 
 
 const bannerRouter = Router();
 
 // Lấy dữ liệu từ db
 
-bannerRouter.get("/layDanhSachBanner", layDanhSachBanner);
+bannerRouter.get("/layDanhSachBanner",kiemTra, layDanhSachBanner);
 
 
 
