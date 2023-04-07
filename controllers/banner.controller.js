53696df7bb69df7451e4804a09c0c55dea3cceb7
Le Nguyen Phuong Thai <thailnp133@gmail.com>
2023-04-07 15:59:57 +0700

    fix and merge

--- controllers/banner.controller.js
+++ controllers/banner.controller.js
@@ -1,5 +1,6 @@
 import bannerModel from "../models/banner.model.js";
 import flimModel from "../models/flim.model.js";
+//------------------------------------------------LẤY DANH SÁCH BANNER--------------------------------
 
 export const layDanhSachBanner = async (req, res, next) => {
   try {
@@ -11,107 +12,6 @@ export const layDanhSachBanner = async (req, res, next) => {
         select: "-_id",
       })
       .lean(); // trả về kiểu js thuần để có thể map dữ liệu
-import bannerModel from "../models/banner.model.js";
-
-//------------------------------------------------PHÂN TRANG BANNER--------------------------------
-export const layDanhSachBannerPhanTrang = async (req, res, next) => {
-  let page = req.query.soTrang;
-  let PAGE_SIZE = req.query.SoPhanTuTrenTrang;
-  if (page) {
-    // Lấy theo phân trang
-    if (page < 1) {
-      page = 1;
-    }
-
-    page = parseInt(page);
-    let soLuongBoQua = (page - 1) * PAGE_SIZE;
-    try {
-      const total = await bannerModel.countDocuments({});
-      if (soLuongBoQua >= total) {
-        return res.json({
-          content: {
-            tongSoTrang: Math.ceil(total / PAGE_SIZE),
-            trangHienTai: page,
-            tongSoLuongHienCo: total,
-            soPhanTuHienTai: 0,
-            data: [],
-          },
-        });
-      }
-      let soLuongPhanTuHienTai = PAGE_SIZE;
-      if (PAGE_SIZE > total) {
-        soLuongPhanTuHienTai = total;
-      } else if (total - soLuongBoQua < PAGE_SIZE) {
-        soLuongPhanTuHienTai = total - soLuongBoQua;
-      }
-      const data = await bannerModel
-        .find({})
-        .skip(soLuongBoQua)
-        .limit(PAGE_SIZE);
-      let tongSoTrang = Math.ceil(total / PAGE_SIZE);
-      if (page > tongSoTrang) {
-        return res.json({
-          content: {
-            tongSoTrang: tongSoTrang,
-            trangHienTai: page,
-            tongSoLuongHienCo: total,
-            soPhanTuHienTai: 0,
-            data: [],
-          },
-        });
-      }
-      res.json({
-        content: {
-          tongSoTrang: tongSoTrang,
-          trangHienTai: page,
-          tongSoLuongHienCo: total,
-          soPhanTuHienTai: parseInt(soLuongPhanTuHienTai),
-          data: data,
-        },
-      });
-    } catch (err) {
-      res.json("Lỗi");
-    }
-  }
-};
-//------------------------------------------------LẤY DANH SÁCH BANNER--------------------------------
-export const layDanhSachBanner = (req, res, next) => {
-  bannerModel
-    .find({})
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.json("Lỗi");
-    });
-};
-//------------------------------------------------THÊM BANNER MỚI--------------------------------
-export const themBanner = (req, res, next) => {
-  const maPhim = req.body.maPhim;
-  const hinhAnh = req.body.hinhAnh;
-  bannerModel
-    .findOne({
-      $or: [{ maPhim: maPhim }, { hinhAnh: hinhAnh }],
-    })
-    .then((banner) => {
-      if (banner) {
-        // username hoặc email đã tồn tại, trả về thông báo tương ứng
-        if (banner.maPhim === tenPhim) {
-          res.status(400).json({ message: "Mã phim đã tồn tại" });
-        } else {
-          res.status(400).json({ message: "Hình ảnh đã tồn tại" });
-        }
-      } else {
-      }
-    })
-    .then((banner) => {
-      res.json({ message: "Thêm mới thành công" });
-    })
-    .catch((error) => {
-      res.status(500).json({ message: "Lỗi" });
-    });
-};
-    
 
     res.json({
       content: {
@@ -121,4 +21,27 @@ export const themBanner = (req, res, next) => {
   } catch (err) {
     res.json("Lỗi");
   }
+};
+
+//------------------------------------------------THÊM BANNER MỚI--------------------------------
+export const themBanner = (req, res, next) => {
+  const maPhim = req.body.maPhim;
+  const hinhAnh = req.body.hinhAnh;
+  bannerModel
+    .findOne({
+      maPhim: parseInt(maPhim),
+    })
+    .then((banner) => {
+      if (banner) {
+        if (banner.maPhim === tenPhim) {
+          res.status(400).json({ message: "Mã phim đã tồn tại" });
+        }
+      } 
+    })
+    .then((banner) => {
+      res.json({ message: "Thêm mới thành công" });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Lỗi" });
+    });
 };
--- routes/banner.routes.js
+++ routes/banner.routes.js
@@ -8,15 +8,7 @@ const bannerRouter = Router();
 
 bannerRouter.get("/layDanhSachBanner", layDanhSachBanner);
 
-import { Router } from "express";
 
-const bannerRouter = Router();
-
-// Lấy dữ liệu từ db
-
-// Gửi dữ liệu vào db
-
-// Cập nhật dữ liệu
 
 bannerRouter.put("/:id", (req, res, next) => {
   var id = req.params.id;
@@ -48,7 +40,5 @@ bannerRouter.delete("/:id", (req, res, next) => {
       res.json("Lỗi");
     });
 });
-
-export default bannerRouter;
 
 export default bannerRouter;
